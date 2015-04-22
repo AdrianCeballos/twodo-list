f1b7e020486f708ca2d5cfefa80f78a25c9c782c
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2015-04-22 10:55:52 -0700

    milestone 7

--- index.php
+++ index.php
@@ -11,10 +11,16 @@ and open the template in the editor.
         <link rel="stylesheet" type="text/css" href="css/main.css"</link> 
     </head>
     <body>
-        <div class = "wrap"></div>
-        
-        <?php
-        // put your code here
-        ?>
+        <div class = "wrap">
+            <div class ="task-list">
+                <ul>
+                    <?php require("includes/connect.php");
+                    ?>
+                </ul>
+            </div>
+        </div>
+        <form class="add-new-task" autocomplete="off">
+            <input type="text" name="new-task" placeholder="Add new item..."/>
+        </form>
     </body>
 </html>
