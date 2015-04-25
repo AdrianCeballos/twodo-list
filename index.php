85ff93354668f9519cd965ad8d8d5a4782c37395
Adrian Ceballos <zzAdrianCeballos@gmail.com>
2015-04-24 19:45:37 -0700

    cant login

--- index.php
+++ index.php
@@ -9,59 +9,43 @@
     </head>
     <body>
         <div class = "wrap">
+            <a href ="view/login-form.php">Login</a>
+            <a href ="view/register-form.php">Register</a>
+            <a href ="view/logout-user.php">Log out</a>
             <div class ="task-list">
                 <ul>
-                    <?php require("includes/connect.php");
-                    $mysqli = new mysqli('localhost','root','root','twodo');
+                    <?php
+                    require_once(__DIR__ . "/controller/login-verify.php");
+                    require_once(__DIR__ . "/view/header.php");
+                    if (authenticateUser()) {
+                        require_once(__DIR__ . "/view/navigation.php");
+                    }
+                    require_once(__DIR__ . "/controller/create-db.php");
+                    require_once(__DIR__ . "/view/footer.php");
+                    require("includes/connect.php");
+                    $mysqli = new mysqli('localhost', 'root', 'root', 'twodo');
                     $query = "SELECT = FROM tasks ORDER BY date ASC, time ASC";
-                    if ($result = $mysqli->query($query)){
-                        $numrows= $result->num_rows;
-                        if ($numrows>0){
-                            while($row = $result->fetch_assoc()){
+                    if ($result = $mysqli->query($query)) {
+                        $numrows = $result->num_rows;
+                        if ($numrows > 0) {
+                            while ($row = $result->fetch_assoc()) {
                                 $task_id = $row['id'];
                                 $task_name = $row['task'];
                                 echo '<li>
-                                <span>'.$task_name.'</span>
-                                  <img id="'.task_id.'" class="delete-button" width = "10px" src="images/close.svg"/>
+                                <span>' . $task_name . '</span>
+                                  <img id="' . task_id . '" class="delete-button" width = "10px" src="images/close.svg"/>
                                 </li>';
                             }
                         }
                     }
-                    
                     ?>
                 </ul>
             </div>
             <form class="add-new-task" autocomplete="off">
-            <input type="text" name="new-task" placeholder="Add new item..."/>
+                <input type="text" name="new-task" placeholder="Add new item..."/>
             </form>
         </div>
-        
+
     </body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <script>
-        add_task();
-        function add_task(){
-            $('.add-new-task').submit(function(){
-                var new_task = $('.add-new-task input[name=new-task]').val();
-                
-                if (new_task != ''){
-                    $.post('includes/add-task.php',{task: new_task}, function(data){
-                        $('.add-new-task input[name=new-task]').val();
-                        $(data).appendTo('.task-list ul').hide().fadeIn();
-                    });
-                }
-                return false;
-            });
-        }
-        $('.delete-button').click(function(){
-            var current_element = $(this);
-            var task_id = $(this).attr('id');
-            $.post('includes/delete-task.php', {id: task_id}, function(){
-            current_element.parent().fadeOut("fast", function(){
-            $(this).remove();    
-           }); 
-        });
-    });
-    </script>
+    
 </html>
